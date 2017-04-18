b71397c86b66f447d7fb1a7ceced321fb7412498
Stephanie Br <stephaniebr@apples-MacBook-Pro-3.local>
2017-04-18 16:38:50 +0530

    indentation fixes

--- js/main.js
+++ js/main.js
@@ -1,15 +1,8 @@
 /*set offset for sticky navbar*/
-    function scrollTo(goto){
-        var offset_top=$(goto).offset().top - 150;
-        $("body,html").stop(true,true).animate({scrollTop:offset_top},300)
-    }
-    $(".anthill-navbar-links .nav-list a").click(function(e) {
-        // Prevent a page reload when a link is pressed
-        e.preventDefault();
-        // Call the scroll function
-        var goto=$(this).attr('data-href');
-        scrollTo(goto);
-    });
+function scrollTo (goto) {
+  var offset_top = $(goto).offset().top - 150;
+  $("body,html").stop(true,true).animate({scrollTop:offset_top},300)
+}
 
 function initLeaflets() {
 
@@ -20,24 +13,23 @@ function initLeaflets() {
 
   /*Map*/
   $('.leaflet-map').each(function initLeafletMap () {
-      var   $container = $(this)
-          , defaults = {
-            zoom: 17
-            , marker: [12.999, 77.699] // bangalore
-            , label: null
-            , maxZoom: 18
-            , attribution: '<a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>'
-            , subdomains: ['a','b','c']
-            , scrollWheelZoom: false
-        }
-        , args
-        , options
-        , map
-        , marker
-        ;
+    var $container = $(this),
+      defaults = {
+        zoom: 17,
+        marker: [12.999, 77.699], // bangalore
+        label: null,
+        maxZoom: 18,
+        attribution: '<a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>',
+        subdomains: ['a','b','c'],
+        scrollWheelZoom: false,
+      },
+      args,
+      options,
+      map,
+      marker;
 
-      // remove any child elements from the container
-      $container.empty();
+    // remove any child elements from the container
+    $container.empty();
 
     args = $container.data();
     if (args.marker) { args.marker = args.marker.split(','); }
@@ -58,12 +50,22 @@ function initLeaflets() {
 
     marker = new L.marker(options.marker).addTo(map);
     if (options.label) marker.bindPopup(options.label).openPopup();
-})
+  });
 }
 
 $(document).ready(function() {
-    initLeaflets();
-})
+
+  initLeaflets();
+
+  $(".anthill-navbar-links .nav-list a").click(function (e) {
+    // Prevent a page reload when a link is pressed
+    e.preventDefault();
+    // Call the scroll function
+    var goto = $(this).attr('data-href');
+    scrollTo(goto);
+  });
+
+});
 
 /*facebook share button*/
 (function(d, s, id) {

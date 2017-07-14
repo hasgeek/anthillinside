eb8f1ea4edc44641f0f41affa2d30a0efb4650a1
Vidya Ramakrishnan <vidya@hasgeek.in>
2017-07-14 18:44:29 +0530

    Add confirmed talks section.

--- js/main.js
+++ js/main.js
@@ -53,6 +53,56 @@ function initLeaflets() {
   });
 }
 
+var updateFontSize = function(elem) {
+  var fontStep = 1;
+  var parentWidth = $(elem).width();
+  var parentHeight = parseInt($(elem).css('max-height'), 10);
+  var childElem = $(elem).find('span');
+  while ((childElem.width() > parentWidth) || (childElem.height() > parentHeight)) {
+    childElem.css('font-size', parseInt(childElem.css('font-size'), 10) - fontStep + 'px');
+  }
+};
+
+var getElemWidth = function(elem) {
+  var card_width = $(elem).css('width');
+  var card_margin = $(elem).css('margin-left');
+  var card_total_width = parseInt(card_width, 10) + 2.5 * parseInt(card_margin, 10);
+  return card_total_width;
+};
+
+var enableScroll = function(items_length) {
+  $(".mCustomScrollbar").css('width', items_length * getElemWidth(".proposal-card") + 'px');
+  $('.mCustomScrollbar').mCustomScrollbar({ axis:"x", theme: "dark-3", scrollInertia: 10, alwaysShowScrollbar: 0});
+};
+
+var parseProposalJson = function(json) {
+  var proposal_ractive = new Ractive({
+    el: '#funnel-proposals',
+    template: '#proposals-wrapper',
+    data: {
+      proposals: json.proposals
+    },
+    complete: function() {
+      $.each($('.proposal-card .title'), function(index, title) {
+        updateFontSize(title);
+      });
+
+      //Set width of content div to enable horizontal scrolling
+      enableScroll(json.proposals.length);
+
+      $(window).resize(function() {
+        enableScroll(json.proposals.length);
+      });
+
+      $('#funnel-proposals .click, #funnel-proposals .btn').click(function(event) {
+        var action = $(this).data('label');
+        var target = $(this).data('target');
+        Fifthel.sendGA('click', action, target);
+      });
+    }
+  });
+};
+
 /*facebook share button*/
 (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
@@ -138,6 +188,19 @@ $(document).ready(function() {
       }
     });
   });
+
+  // For confirmed proposals card
+  //If funnel-proposals divs is present on the page, then make the ajax call.
+  if(($('#funnel-proposals').length)) {
+    $.ajax({
+      type: 'GET',
+      dataType: 'jsonp',
+      url: 'https://anthillinside.talkfunnel.com/2017/json',
+      success: function(data) {
+        parseProposalJson(data);
+      }
+    });//eof ajax call
+  }
 
 });
 
